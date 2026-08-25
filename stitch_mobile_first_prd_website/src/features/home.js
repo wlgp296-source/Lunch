86d17a7061fdc00d53f3690175e257c5ac2f30e3
박지혜 <wlgp296@gmail.com>
2026-08-25 15:18:44 +0900

    Animate home roulette idle state

--- stitch_mobile_first_prd_website/src/features/home.js
+++ stitch_mobile_first_prd_website/src/features/home.js
@@ -5,7 +5,7 @@ export function renderHome() {
     <section class="home-screen">
       <div class="home-decor decor-left">🥟</div><div class="home-decor decor-right">🥗</div>
       <div class="brand"><h1>Lunch<br />Roulette</h1><p>오늘 뭐 먹지?</p></div>
-      <div class="roulette-placeholder"><div class="pointer"></div><div class="roulette-center">Spin</div></div>
+      <div class="roulette-placeholder home-wheel" aria-label="천천히 돌아가는 점심 룰렛"><div class="pointer"></div><div class="roulette-center">Spin</div></div>
       <div class="home-actions">
         <button class="primary-button action-button" data-action="team-create"><span>${icon('group')}<b>팀 점심방 만들기</b></span>${icon('arrow')}</button>
         <button class="secondary-button action-button" data-action="solo"><span>${icon('person')}<b>혼자 추천받기</b></span>${icon('arrow')}</button>
